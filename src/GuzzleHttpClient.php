753bfa75587a7579e03dbe6a47b410356cfa832a
An Cao <an.cao@haltian.com>
2017-09-05 08:59:05 +0300

    HttpClient

--- src/GuzzleHttpClient.php
+++ src/GuzzleHttpClient.php
@@ -4,11 +4,25 @@ declare(strict_types=1);
 
 namespace Outplay\RiotApi;
 
+use GuzzleHttp\Client;
 use Psr\Http\Message\ResponseInterface;
 
 final class GuzzleHttpClient implements Interfaces\HttpClient
 {
+    protected $client;
+
+    public function __construct()
+    {
+        $this->setClient(new Client());
+    }
+
+    public function setClient(Client $client)
+    {
+        $this->client = $client;
+    }
+
     public function fetch(string $url) : ResponseInterface
     {
+        return $this->client->get($url);
     }
 }
